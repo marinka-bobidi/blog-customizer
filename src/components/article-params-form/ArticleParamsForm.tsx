5c17c6af2fc2b980d55f8fb419e5e8a388217d18
marinka-bobidi <155455955+marinka-bobidi@users.noreply.github.com>
2024-09-28 01:46:01 +0300

    reviewNew

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,124 @@
 import { ArrowButton } from 'components/arrow-button';
+import { Text } from 'components/text';
+import { Select } from 'components/select/';
 import { Button } from 'components/button';
-
+import { ArticleStateType, OptionType } from 'src/constants/articleProps';
 import styles from './ArticleParamsForm.module.scss';
+import React, { FormEvent, useState } from 'react';
+import clsx from 'clsx';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
 
-export const ArticleParamsForm = () => {
+interface ArticleParamsFormProps {
+	isFixedOpen?: boolean;
+	ParamsState: ArticleStateType;
+	setApp: (el: ArticleStateType) => void;
+}
+
+export const ArticleParamsForm: React.FC<ArticleParamsFormProps> = ({
+	isFixedOpen = false,
+	ParamsState,
+	setApp,
+}) => {
+	const [formState, setFormState] = useState<ArticleStateType>(ParamsState);
+
+	const setFormStateFunc = (value: OptionType, name: string) => {
+		const new_obj = {
+			...formState,
+			[name]: value,
+		};
+		setFormState(new_obj);
+	};
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	const togglePanel = () => {
+		setIsOpen(!isOpen);
+	};
+	const handleSubmit = (event: FormEvent) => {
+		setApp(formState);
+		event.preventDefault();
+	};
+
+	const resetSubmit = () => {
+		setFormState(ParamsState);
+		setApp(ParamsState);
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={togglePanel} isOpen={isOpen} />
+			<div
+				className={
+					isOpen
+						? clsx(styles.overlay_open, styles.overlay)
+						: clsx(styles.overlay)
+				}
+				onClick={togglePanel}></div>
+			<aside
+				className={
+					isOpen
+						? clsx([styles.container_open, styles.container])
+						: clsx([styles.container])
+				}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => {
+						handleSubmit(e);
+					}}>
+					<Text uppercase={true} weight={800} size={31}>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={formState.fontFamilyOption}
+						onChange={(font) => {
+							setFormStateFunc(font, 'fontFamilyOption');
+						}}
+						options={fontFamilyOptions}
+						title='РАЗМЕР ШРИФТА'
+					/>
+					<RadioGroup
+						name='font size'
+						selected={formState.fontSizeOption}
+						onChange={(size) => {
+							setFormStateFunc(size, 'fontSizeOption');
+						}}
+						options={fontSizeOptions}
+						title='РАЗМЕР ШРИФТА'></RadioGroup>
+					<Select
+						selected={formState.fontColor}
+						onChange={(color) => {
+							setFormStateFunc(color, 'fontColor');
+						}}
+						options={fontColors}
+						title='ЦВЕТ ШРИФТА'
+					/>
+					<Separator />
+					<Select
+						selected={formState.backgroundColor}
+						onChange={(color) => {
+							setFormStateFunc(color, 'backgroundColor');
+						}}
+						options={backgroundColors}
+						title='ЦВЕТ ФОНА'
+					/>
+					<Select
+						selected={formState.contentWidth}
+						onChange={(width) => {
+							setFormStateFunc(width, 'contentWidth');
+						}}
+						options={contentWidthArr}
+						title='ШИРИНА КОНТЕНТА'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button title='Сбросить' type='reset' onClick={resetSubmit} />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -3,8 +3,18 @@ import clsx from 'clsx';
 // Сообщаем вебпаку, что этот файл использует это изображение.
 import plane from 'src/images/plane.png';
 import { Text } from 'components/text';
-
+import { OptionType } from 'src/constants/articleProps';
 import styles from './Article.module.scss';
+
+interface ArticleType {
+	articleState: {
+		fontFamilyOption: OptionType;
+		fontColor: OptionType;
+		backgroundColor: OptionType;
+		contentWidth: OptionType;
+		fontSizeOption: OptionType;
+	};
+}
 
 export const Article = () => {
 	return (
